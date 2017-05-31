70b29bd6d5a5b0ef1a65cc1fbea0a24d40f55665
Jianyang Zhang <jianyang212@gmail.com>
2017-05-31 15:47:17 -0700

    Driver, Connector
    Driver, Connector

--- src/main/resources/java/BrowserDriver.java
+++ src/main/resources/java/BrowserDriver.java
@@ -1,24 +1,41 @@
+import java.util.Iterator;
+import java.util.List;
 import java.io.File;
+import java.io.IOException;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.pagefactory.ElementLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class BrowserDriver {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\jiany\\emailcrawlers\\chromedriver.exe");
 		WebDriver dr = new ChromeDriver(enableExtension("C:\\\\Users\\jiany\\emailcrawlers\\hunter.crx"));
+		// System.setProperty("webdriver.chrome.driver",
+		// "/Applications/chromedriver");
+		// WebDriver dr = new
+		// ChromeDriver(enableExtension("/Users/Wentao/Documents/hunter.crx"));
+
 		dr.get("http://www.linkedin.com");
-		// dr.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		switchTab("LinkedIn: Log In or Sign Up", dr);
-		signInLinkedin("velozproject@gmail.com", "19940916", dr);
+		signInLinkedin("wangwent@usc.edu", "19940916", dr);
+		searchKeyword("facebook", dr);
 	}
-	
+
 	/**
 	 * use extension in the Chrome browser
 	 **/
@@ -29,9 +46,9 @@ public class BrowserDriver {
 		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 		return capabilities;
 	}
-	
+
 	/**
-	 * switch tab based on tab name 
+	 * switch tab based on tab name
 	 */
 	public static boolean switchTab(String tabName, WebDriver dr) {
 		boolean flag = false;
@@ -56,11 +73,53 @@ public class BrowserDriver {
 	}
 	
 	/**
-	 * Linkedin login 
+	 * login Hunter
 	 */
-	public static void signInLinkedin(String userName, String password, WebDriver dr){
+	public static void signInHunter(String userName, String password, WebDriver dr) {
 		dr.findElement(By.name("session_key")).sendKeys(userName);
 		dr.findElement(By.name("session_password")).sendKeys(password);
 		dr.findElement(By.id("login-submit")).click();
+	}
+
+	/**
+	 * login Linkedin
+	 */
+	public static void signInLinkedin(String userName, String password, WebDriver dr) {
+		dr.findElement(By.name("session_key")).sendKeys(userName);
+		dr.findElement(By.name("session_password")).sendKeys(password);
+		dr.findElement(By.id("login-submit")).click();
+	}
+
+	/**
+	 * input the search keyword and get page source
+	 * 
+	 * @throws InterruptedException
+	 */
+	public static void searchKeyword(String title, WebDriver dr) throws IOException, InterruptedException {
+		String url = "https://www.linkedin.com/search/results/index/?keywords=" + title
+				+ "&origin=GLOBAL_SEARCH_HEADER";
+		dr.get(url);
+		JavascriptExecutor jse = (JavascriptExecutor) dr;
+		jse.executeScript("scroll(0, 1000);");
+
+		String page = dr.getPageSource();
+		System.out.println(page);
+
+		Document doc = Jsoup.parse(page);
+		Elements uls = doc.getElementsByTag("ul");
+		for (Element ul : uls) {
+			Elements lists = ul.getElementsByTag("li");
+			for (Element li : lists) {
+				try {
+					Elements aTags = li.getElementsByTag("a");
+					for (Element aTag : aTags) {
+						String href = aTag.attr("href");
+						System.out.println("href is " + href);
+					}
+				} catch (Exception e) {
+
+				}
+			}
+		}
 	}
 }
---
+++ src/main/resources/java/MySQLConnector.java
@@ -0,0 +1,54 @@
+import java.sql.*;
+
+/**
+ * Connect to MySQL database and execute queries.
+ **/
+public class MySQLConnector {
+	public static void main(String[] args) {
+		// tester
+		Connection connection = MySQLConnector.createConnection("transcript", "root", "");
+		ResultSet resultSet = MySQLConnector.executeQuery(connection, "select * from student");
+		MySQLConnector.printResultSet(resultSet);
+	}
+
+	public static Connection createConnection(String schema, String username, String password) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + schema, username,
+					password);
+			return connection;
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+
+	public static ResultSet executeQuery(Connection connection, String query) {
+		try {
+			Statement stmt = connection.createStatement();
+			return stmt.executeQuery(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void printResultSet(ResultSet resultSet) {
+		try {
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			int columnsNumber = metaData.getColumnCount();
+			while (resultSet.next()) {
+				for (int i = 1; i <= columnsNumber; i++) {
+					if (i > 1) {
+						System.out.print(",  ");
+					}
+					String columnValue = resultSet.getString(i);
+					System.out.print(metaData.getColumnName(i) + ": " + columnValue);
+				}
+				System.out.println("");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
