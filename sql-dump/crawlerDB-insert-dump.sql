7c9a7d0051962c804504b909f99ec6e26bdba739
Wentao Wang <wentao.tju@gmail.com>
2017-06-23 11:57:17 -0700

    database

--- sql-dump/crawlerDB-insert-dump.sql
+++ sql-dump/crawlerDB-insert-dump.sql
@@ -31,23 +31,31 @@ Insert into Compaign VALUES('0','CT','1');
 Insert into Customer VALUES('testurl1','Kobe','ceo','los angeles','ceo','2017-06-18','2017-06-19','CT','0');
 Insert into Customer VALUES('testurl2','James','ceo','cleveland','ceo','2017-06-18','2017-06-19','CT','0');
 Insert into Customer VALUES('testurl3','Tim','ceo','texas','ceo','2017-06-18','2017-06-19','CT','0');
+Insert into Customer VALUES('testurl4','Tony','ceo','texas','ceo','2017-06-18','2017-06-19','CT','0');
+
 Insert into Company VALUES('baller','baller.com');
 insert into Employment VALUES('testurl1','baller');
 insert into Employment VALUES('testurl2','baller');
 insert into Employment VALUES('testurl3','baller');
+insert into Employment VALUES('testurl4','baller');
+
 insert into Email VALUES('kobe@baller.com','testurl1','baller','0');
 insert into Email VALUES('kobe24@baller.com','testurl1','baller','0');
 insert into Email VALUES('kobe8@baller.com','testurl1','baller','0');
+
+
+
 insert into Email VALUES('james@baller.com','testurl2','baller','0');
 insert into Email VALUES('james23@baller.com','testurl2','baller','0');
 insert into Email VALUES('james6@baller.com','testurl2','baller','0');
+
+
 insert into Email VALUES('tim@baller.com','testurl3','baller','2');
 insert into Result Values('20170621234247153','testurl1');
 Insert into Result Values('20170621234247153','testurl2');
 Insert into Result Values('20170621234247153','testurl3');
-insert into Result Values('20170621234247153','testurl1');
-Insert into Result Values('20170621234247153','testurl2');
-Insert into Result Values('20170621234247153','testurl3');
+Insert into Result Values('20170621234247153','testurl4');
+
 
 update Search set search_progress="completed" where search_id="20170621234247153";
 update Search set search_progress="processing" where search_id="20170621234436192";
