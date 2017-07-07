a9023eaa01ee35e36c6d919ed33049d515e067d7
Jianyang Zhang <jianyang212@gmail.com>
2017-07-07 16:45:22 -0700

    salesgenie
    salesgenie

--- src/main/resources/java/crawler/service/DriveSalesgenieService.java
+++ src/main/resources/java/crawler/service/DriveSalesgenieService.java
@@ -29,35 +29,50 @@ public class DriveSalesgenieService extends DriveBrowserService{
 		dr.findElement(By.id("formSignIn")).click();
 		try {
 			dr.findElement(By.id("submit")).click();
-			Thread.sleep(4000);
-			WebElement skipAlert = dr.findElement(By.xpath("//div[contains(@class, 'walkme-x-button')]"));
-			skipAlert.click();
-			Thread.sleep(1000);
+			WebDriverWait wait = new WebDriverWait(dr, 40);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'walkme-x-button')]")));
+			dr.findElement(By.xpath("//div[contains(@class, 'walkme-x-button')]")).click();
 		} catch (Exception e) {
+			System.out.println("something wrong in method 'signInSalesgenie'");
 		}
 	}
 	
-	private String waitCurrentTableLoad() {
+	private String waitTillTableLoad() {
 		WebDriverWait wait = new WebDriverWait(dr, 40);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("grid-view-table-container")));
 		return dr.getPageSource();
 	}
 	
 	protected void crawlCurrentTable() {
-		String page = waitCurrentTableLoad();
+		String page = waitTillTableLoad();
 		Document doc = Jsoup.parse(page);
 		Elements elements_odd_tr = doc.select("tr.odd");
 		Elements elements_even_tr = doc.select("tr.even");
 		for (Element element : elements_odd_tr) {
-			String companyName = element.select("div.BusinessName").text().trim();
-			if (!companyName.equals("")) {
-				System.out.println(companyName);
-			}
+			extractTableInfo(element);
 		}
 		for (Element element : elements_even_tr) {
+			extractTableInfo(element);
+		}
+	}
+	
+	private void extractTableInfo(Element element) {
+		if (element.children().size() > 10) {
 			String companyName = element.select("div.BusinessName").text().trim();
-			if (!companyName.equals("")) {
-				System.out.println(companyName);
+			String personName = element.select("div.executive-name-field").text().trim();
+			String phoneNumber = element.select("div[data-is-wireless]").attr("data-phone").trim();
+			String street = element.select("td").get(8).text().trim();
+			String city = element.select("td").get(9).text().trim();
+			String state = element.select("td").get(10).text().trim();
+			String zipCode = element.select("td").get(11).text().trim();
+			if (!personName.equals("")) {
+				System.out.print(companyName + " ");
+				System.out.print(personName + " ");
+				System.out.print(phoneNumber + " ");
+				System.out.print(street + " ");
+				System.out.print(city + " ");
+				System.out.print(state + " ");
+				System.out.println(zipCode + " ");
 			}
 		}
 	}
